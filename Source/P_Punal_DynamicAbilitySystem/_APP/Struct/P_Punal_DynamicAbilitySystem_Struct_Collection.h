d04bbb64865b15081985ece68395f4296c024fce
Punal Manalan <punal100@gmail.com>
2025-04-22 17:51:03 +0530

    WIP Added Multiple Structs in P_Punal_DynamicAbilitySystem_Struct_Collection.h
    Summary:
    Added Multiple Structs in P_Punal_DynamicAbilitySystem_Struct_Collection.h
    
    Changes:
    1. Modified Headers in P_Punal_DynamicAbilitySystem_Struct_Collection.h, Specifically Added "../Enum/P_Punal_DynamicAbilitySystem_Enum_Collection.h" Header.
    2. Added FStruct_PDAS_Attribute_With_Modifier::{Struct} C++ Struct in P_Punal_DynamicAbilitySystem_Struct_Collection.h.
    3. Added FStruct_PDAS_Attribute_With_Modifier_Named::{Struct} C++ Struct in P_Punal_DynamicAbilitySystem_Struct_Collection.h.
    4. Added FStruct_PDAS_Attribute_With_Modifiers_Map_By_Name::{Struct} C++ Struct in P_Punal_DynamicAbilitySystem_Struct_Collection.h.
    
    Tests:
    1. Tests Done, Working As Intended.
    
    Task: C++ Structure PDAS
    
    End Of Update
    
    Signed-off-by: Punal Manalan <punal100@gmail.com>

--- Source/P_Punal_DynamicAbilitySystem/_APP/Struct/P_Punal_DynamicAbilitySystem_Struct_Collection.h
+++ Source/P_Punal_DynamicAbilitySystem/_APP/Struct/P_Punal_DynamicAbilitySystem_Struct_Collection.h
@@ -7,12 +7,9 @@
 #endif // !DEFINED_HEADER_P_Punal_DynamicAbilitySystem_Struct_Collection
 
 #include "CoreMinimal.h"
-//#include "GameplayEffect.h"
-////#include "GameplayEffectExtension.h"
-//#include "Abilities/GameplayAbility.h"
-//#ifndef DEFINED_HEADER_SOME_HEADER_MARCO_IDENTIFIER
-//#include "../SOME_HEADER.h"
-//#endif // !DEFINED_HEADER_SOME_HEADER_MARCO_IDENTIFIER
+#ifndef DEFINED_HEADER_P_Punal_DynamicAbilitySystem_Enum_Collection
+#include "../Enum/P_Punal_DynamicAbilitySystem_Enum_Collection.h"
+#endif // !DEFINED_HEADER_P_Punal_DynamicAbilitySystem_Enum_Collection
 #include "P_Punal_DynamicAbilitySystem_Struct_Collection.generated.h"
 
 USTRUCT(BlueprintType)
@@ -38,6 +35,36 @@ public:
 	//Punal Manalan, NOTE: Current Value of Attribute in float
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_APP|Struct|PDAS|Attribute")
 	float Current = 0.0f;
+
+	//#==
+	//#-- Variables-Base #--
+	//#- Variables #-
+	//~ ~
+};
+
+USTRUCT(BlueprintType)
+struct FStruct_PDAS_Attribute_With_Modifier : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	//~ ~
+	//#+ Variables #+
+	//#++ Variables-Base #++
+	//#==
+
+	//Punal Manalan,
+	//NOTE: 
+	//Contains Attribute Modifier and Attribute Value
+
+	//Punal Manalan, NOTE: Attribute Name
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_APP|Struct|PDAS|Attribute")
+	EEnum_PDAS_Math_Operation Modifier = EEnum_PDAS_Math_Operation::Add;
+
+	//Punal Manalan, NOTE: Attribute Value
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_APP|Struct|PDAS|Attribute")
+	FStruct_PDAS_Attribute Attribute;
 
 	//#==
 	//#-- Variables-Base #--
@@ -76,6 +103,36 @@ public:
 };
 
 USTRUCT(BlueprintType)
+struct FStruct_PDAS_Attribute_With_Modifier_Named : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	//~ ~
+	//#+ Variables #+
+	//#++ Variables-Base #++
+	//#==
+
+	//Punal Manalan,
+	//NOTE: 
+	//Contains Attribute Name and Attribute Value
+
+	//Punal Manalan, NOTE: Attribute Name
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_APP|Struct|PDAS|Attribute")
+	FString Name = "Default";
+
+	//Punal Manalan, NOTE: Attribute Value
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_APP|Struct|PDAS|Attribute")
+	FStruct_PDAS_Attribute_With_Modifier Attribute;
+
+	//#==
+	//#-- Variables-Base #--
+	//#- Variables #-
+	//~ ~
+};
+
+USTRUCT(BlueprintType)
 struct FStruct_PDAS_Attributes_Map_By_Name : public FTableRowBase
 {
 	GENERATED_BODY()
@@ -94,6 +151,32 @@ public:
 	//Punal Manalan, NOTE: Attributes Map by Name(FString)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_APP|Struct|PDAS|Attribute")
 	TMap<FString,FStruct_PDAS_Attribute> Attributes;
+
+	//#==
+	//#-- Variables-Base #--
+	//#- Variables #-
+	//~ ~
+};
+
+USTRUCT(BlueprintType)
+struct P_PUNAL_DYNAMICABILITYSYSTEM_API FStruct_PDAS_Attribute_With_Modifiers_Map_By_Name : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+
+	//~ ~
+	//#+ Variables #+
+	//#++ Variables-Base #++
+	//#==
+
+	//Punal Manalan,
+	//NOTE: 
+	//Contains Map of Attributes with Modifier Map by Name(FString)
+
+	//Punal Manalan, NOTE: Attributes with Modifier Map by Name(FString)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "_APP|Struct|PDAS|Attribute")
+	TMap<FString, FStruct_PDAS_Attribute_With_Modifier> Attributes;
 
 	//#==
 	//#-- Variables-Base #--
